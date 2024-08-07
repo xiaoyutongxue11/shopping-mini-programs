6c295ed3678624daeea5e90cee2add5213a35fe7
tyy123 <1572235056@qq.com>
2024-08-07 20:39:59 +0800

    🎉 ：项目初始化、uni-ui组件库配置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,6 +4,13 @@
 
 <template>
   <view class="index">index</view>
+  <uni-card
+    title="基础卡片"
+    sub-title="副标题"
+    extra="额外信息"
+    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+  >
+  </uni-card>
 </template>
 
 <style lang="scss">
