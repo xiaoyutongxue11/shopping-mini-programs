ed8cb4c9a4eab74c750d61a4c8b26c3983b8eb3e
tyy123 <1572235056@qq.com>
2024-08-11 15:54:13 +0800

    Swiper

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
-//
+import CustomNavbar from './components/CustomNavbar.vue'
+import XtxSwiper from '@/components/XtxSwiper.vue'
 </script>
 
 <template>
-  <view class="index">index</view>
-  <uni-card
-    title="基础卡片"
-    sub-title="副标题"
-    extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-  >
-  </uni-card>
+  <CustomNavbar />
+  <XtxSwiper />
 </template>
 
 <style lang="scss">
