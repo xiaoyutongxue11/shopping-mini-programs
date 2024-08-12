da151b8cda90ae851fef56a624aa6e87087836b7
tyy123 <1572235056@qq.com>
2024-08-12 17:19:50 +0800

    热门推荐

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -15,3 +15,21 @@ export type PageParams = {
   page?: number
   pageSize?: number
 }
+
+/** 通用商品类型 */
+export type GoodsItem = {
+  /** 商品描述 */
+  desc: string
+  /** 商品折扣 */
+  discount: number
+  /** id */
+  id: string
+  /** 商品名称 */
+  name: string
+  /** 商品已下单数量 */
+  orderNum: number
+  /** 商品图片 */
+  picture: string
+  /** 商品价格 */
+  price: number
+}
