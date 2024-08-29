dcd40aa195df019b93a83c6dc029ec981da511d9
tyy123 <1572235056@qq.com>
2024-08-29 16:19:20 +0800

    订单物流

--- src/services/pay.ts
+++ src/services/pay.ts
@@ -1,3 +1,4 @@
+import type { OrderLogisticResult, OrderResult } from '@/types/order'
 import { http } from '@/utils/http'
 
 export const getPayWxPayMiniPayAPI = (data: { orderId: string }) => {
@@ -21,5 +22,29 @@ export const getMemberOrderConsignmentByIdAPI = (id: string) => {
   return http({
     method: 'GET',
     url: `/member/order/consignment/${id}`,
+  })
+}
+
+/**
+ * 确认收货
+ * @description 仅在订单状态为待收货时，可确认收货。
+ * @param id 订单id
+ */
+export const putMemberOrderReceiptByIdAPI = (id: string) => {
+  return http<OrderResult>({
+    method: 'PUT',
+    url: `/member/order/${id}/receipt`,
+  })
+}
+
+/**
+ * 获取订单物流
+ * @description 仅在订单状态为待收货，待评价，已完成时，可获取物流信息。
+ * @param id 订单id
+ */
+export const getMemberOrderLogisticsByIdAPI = (id: string) => {
+  return http<OrderLogisticResult>({
+    method: 'GET',
+    url: `/member/order/${id}/logistics`,
   })
 }
